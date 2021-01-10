b825d1b7594c43b36d976a696f41dda96ef52615
Jordan Weatherford <jrw821@gmail.com>
2021-01-10 12:31:00 -0800

    stuffs

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ app.use(cors())
 
 // serve up production assets
 app.use(express.static('build'));
-// let the react app to handle any unknown routes 
-// serve up the index.html if express does'nt recognize the route
-const path = require('path');
 
 app.get('*', (req, res) => {
     res.sendFile('index.html', { root: __dirname + '/build/'});
